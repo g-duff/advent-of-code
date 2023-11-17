0bc447053be8bbf2888c33ed5b26278f35c9f38a
George Duffett <gduffett@certinia.com>
2023-11-17 09:35:45 +0000

    feat: day 03 pt 1

---
+++ golang/2023/day03/go.mod
@@ -0,0 +1,3 @@
+module adventofcode.com/day03
+
+go 1.21.4
---
+++ golang/2023/day03/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "strings"
+    "os"
+)
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lettersToPriorities := map[rune]int {
+		'a': 1,
+		'b': 2,
+		'c': 3,
+		'd': 4,
+		'e': 5,
+		'f': 6,
+		'g': 7,
+		'h': 8,
+		'i': 9,
+		'j': 10,
+		'k': 11,
+		'l': 12,
+		'm': 13,
+		'n': 14,
+		'o': 15,
+		'p': 16,
+		'q': 17,
+		'r': 18,
+		's': 19,
+		't': 20,
+		'u': 21,
+		'v': 22,
+		'w': 23,
+		'x': 24,
+		'y': 25,
+		'z': 26,
+		'A': 27,
+		'B': 28,
+		'C': 29,
+		'D': 30,
+		'E': 31,
+		'F': 32,
+		'G': 33,
+		'H': 34,
+		'I': 35,
+		'J': 36,
+		'K': 37,
+		'L': 38,
+		'M': 39,
+		'N': 40,
+		'O': 41,
+		'P': 42,
+		'Q': 43,
+		'R': 44,
+		'S': 45,
+		'T': 46,
+		'U': 47,
+		'V': 48,
+		'W': 49,
+		'X': 50,
+		'Y': 51,
+		'Z': 52,
+	}
+
+	total := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		upTo := len(line) / 2
+		firstCompartment := line[:upTo]
+		secondCompartment := line[upTo:]
+		fmt.Println(firstCompartment)
+		fmt.Println(secondCompartment)
+
+		for i, r := range firstCompartment {
+			if (!strings.ContainsRune(firstCompartment[i+1:], r) && strings.ContainsRune(secondCompartment, r)) {
+				fmt.Println(string(r))
+				fmt.Println(lettersToPriorities[r])
+				total += lettersToPriorities[r]
+			}
+		}
+	}
+
+	fmt.Println(total);
+}
