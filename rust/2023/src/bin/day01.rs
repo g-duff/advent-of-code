748977a88b2341abdb39c068bc543f8de89ddb2b
George Duffett <gduffett@certinia.com>
2023-11-23 22:15:45 +0000

    feat(rust2022): day01 pt2

--- rust/2023/src/bin/day01.rs
+++ rust/2023/src/bin/day01.rs
@@ -2,14 +2,16 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let input_path = "./data/01.input";
+    let file = File::open("./data/01.input").unwrap();
 
-    let file = File::open(input_path).unwrap();
-    let pt1_ans = pt1(file);
+    // let pt1_ans = pt1(file);
+    // println!("Part 1: {}", pt1_ans);
 
-    println!("Part 1: {}", pt1_ans);
+    let pt2_ans = pt2(file);
+    println!("Part 2: {}", pt2_ans);
 }
 
+#[allow(dead_code)]
 fn pt1(file: File) -> i32 {
     let mut tot = 0;
     let mut max = 0;
@@ -27,4 +29,27 @@ fn pt1(file: File) -> i32 {
     });
 
     max
+}
+
+fn pt2(file: File) -> i32 {
+    let mut tot = 0;
+    let mut max = vec![0, 0, 0];
+
+    BufReader::new(file).lines().for_each(|line| {
+        let val = line.unwrap();
+        if val == "" {
+            for i in 0..max.len() {
+                if tot > max[i] {
+                    max.pop();
+                    max.insert(i, tot);
+                    break;
+                }
+            }
+            tot = 0;
+        } else {
+            tot += val.parse::<i32>().unwrap();
+        }
+    });
+
+    max.into_iter().sum()
 }
