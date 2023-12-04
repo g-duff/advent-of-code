d21c3639145c829d0fd1dc674fa632372941fb01
George Duffett <gduffett@certinia.com>
2023-12-04 16:02:47 +0000

    feat(rust2023): day04 pt01

---
+++ rust/2023/src/bin/day04.rs
@@ -0,0 +1,117 @@
+use std::str;
+
+fn main() {
+    let cards: Vec<Card> = aoc::load_to_vec("./data/04.input".to_string());
+
+    let pt1_answer = solve_pt1(cards);
+
+    println!("Part 1: {pt1_answer}");
+}
+
+fn solve_pt1(cards: Vec<Card>) -> i32 {
+    cards
+        .iter()
+        .map(|c| {
+            let wins = c.count_wins();
+            if wins > 0 {
+                2_i32.pow(wins - 1)
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Card {
+    winning_numbers: Vec<i32>,
+    numbers_you_have: Vec<i32>,
+}
+
+impl Card {
+    fn count_wins(&self) -> u32 {
+        self.numbers_you_have
+            .iter()
+            .map(|n| self.winning_numbers.contains(n) as u32)
+            .sum::<u32>()
+    }
+}
+
+#[derive(Debug)]
+struct ParseCardError;
+
+impl str::FromStr for Card {
+    type Err = ParseCardError;
+
+    fn from_str(s: &str) -> Result<Card, ParseCardError> {
+        let (_card, numbers) = s.split_once(":").unwrap();
+
+        let (win, you) = numbers.split_once("|").unwrap();
+
+        let winning_numbers = win
+            .trim_end()
+            .chars()
+            .collect::<Vec<char>>()
+            .chunks(3)
+            .map(|c| c.into_iter().collect::<String>().trim().parse().unwrap())
+            .collect();
+
+        let numbers_you_have = you
+            .trim_end()
+            .chars()
+            .collect::<Vec<char>>()
+            .chunks(3)
+            .map(|c| c.into_iter().collect::<String>().trim().parse().unwrap())
+            .collect();
+
+        Ok(Card {
+            numbers_you_have,
+            winning_numbers,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[rustfmt::skip]
+    const PT1_INPUT: &str = 
+"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+    #[test]
+    fn parses_input() {
+        // Given
+        let first_card = PT1_INPUT.lines().next().unwrap();
+
+        // When
+        let card: Card = first_card.parse().unwrap();
+
+        // Then
+        assert_eq!(
+            card,
+            Card {
+                winning_numbers: vec![41, 48, 83, 86, 17],
+                numbers_you_have: vec![83, 86, 6, 31, 17, 9, 48, 53]
+            }
+        );
+    }
+
+    #[test]
+    fn solves_pt1() {
+        // Given
+        let cards: Vec<Card> = PT1_INPUT.lines().map(|l| l.parse().unwrap()).collect();
+
+        // When
+        let answer = solve_pt1(cards);
+
+        // Then
+        assert_eq!(answer, 13);
+    }
+}
