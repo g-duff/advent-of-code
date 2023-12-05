76d7693a9bdc436c78ac9c094040bd2b99f515bc
George Duffett <gduffett@certinia.com>
2023-12-05 19:44:11 +0000

    feat(rust2023): day05 pt1

--- rust/2023/src/bin/day05.rs
+++ rust/2023/src/bin/day05.rs
@@ -1,10 +1,39 @@
-use std::str;
+use std::{fs, ops, str};
 
-fn main() {}
+fn main() {
+
+    let almanac: Almanac = fs::read_to_string("./data/05.input")
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    let pt1_answer = solve_pt1(almanac);
+    println!("{pt1_answer}");
+}
+
+fn solve_pt1(almanac: Almanac) -> i64 {
+    let seeds = almanac.seeds;
+
+    let answer: Vec<i64> = seeds.iter().map(|s| {
+        let mut a = s.clone();
+        for m in &almanac.maps {
+            for t in &m.transforms {
+                if t.source_range.contains(&a) {
+                    a = a + t.offset;
+                    break
+                }
+            }
+        }
+        a
+    }).collect();
+
+    let minval = answer.iter().min().unwrap();
+    *minval
+}
 
 #[derive(Debug, PartialEq, Eq)]
 struct Almanac {
-    seeds: Vec<i32>,
+    seeds: Vec<i64>,
     maps: Vec<Map>,
 }
 
@@ -33,7 +62,10 @@ impl str::FromStr for Almanac {
 }
 
 #[derive(Debug, PartialEq, Eq)]
-struct Map {}
+struct Map {
+    label: String,
+    transforms: Vec<Transform>,
+}
 
 #[derive(Debug, PartialEq, Eq)]
 struct ParseMapError;
@@ -41,9 +73,31 @@ struct ParseMapError;
 impl str::FromStr for Map {
     type Err = ParseMapError;
 
-    fn from_str(_input: &str) -> Result<Map, ParseMapError> {
-        Ok(Map {})
+    fn from_str(input: &str) -> Result<Map, ParseMapError> {
+        let mut lines = input.lines();
+
+        let label = lines.next().unwrap().to_string();
+
+        let transforms = input
+            .lines()
+            .skip(1)
+            .map(|t| t.split(' ').map(|i| i.parse::<i64>().unwrap()).collect())
+            .map(|t: Vec<i64>| Transform {
+                source_range: ops::Range {
+                    start: t[1],
+                    end: t[1] + t[2],
+                },
+                offset: t[0] - t[1],
+            })
+            .collect();
+        Ok(Map { label, transforms })
     }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Transform {
+    source_range: ops::Range<i64>,
+    offset: i64,
 }
 
 #[cfg(test)]
@@ -90,15 +144,72 @@ humidity-to-location map:
     #[test]
     fn parses_input() {
         // Given
-        let almanac: Almanac = INPUT.parse().unwrap();
+        let input = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15";
+
+        let almanac: Almanac = input.parse().unwrap();
 
         // Then
         assert_eq!(
             almanac,
             Almanac {
                 seeds: vec![79, 14, 55, 13],
-                maps: vec![Map {}, Map {}, Map {}, Map {}, Map {}, Map {}, Map {},],
+                maps: vec![
+                    Map {
+                        label: "seed-to-soil map:".to_string(),
+                        transforms: vec![
+                            Transform {
+                                source_range: ops::Range {
+                                    start: 98,
+                                    end: 100,
+                                },
+                                offset: -48,
+                            },
+                            Transform {
+                                source_range: ops::Range { start: 50, end: 98 },
+                                offset: 2,
+                            },
+                        ],
+                    },
+                    Map {
+                        label: "soil-to-fertilizer map:".to_string(),
+                        transforms: vec![
+                            Transform {
+                                source_range: ops::Range { start: 15, end: 52 },
+                                offset: -15
+                            },
+                            Transform {
+                                source_range: ops::Range { start: 52, end: 54 },
+                                offset: -15
+                            },
+                            Transform {
+                                source_range: ops::Range { start: 0, end: 15 },
+                                offset: 39
+                            }
+                        ]
+                    }
+                ]
             }
         );
+    }
+
+    #[test]
+    fn solves_pt1() {
+        // Given
+        let input = INPUT.parse().unwrap();
+
+        // When
+        let answer = solve_pt1(input);
+
+        // Then
+        assert_eq!(answer, 35)
     }
 }
