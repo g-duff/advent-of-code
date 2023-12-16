0f7fa5d7568bfce7c464e955d99331de40c8d40c
George Duffett <gduffett@certinia.com>
2023-12-16 15:35:47 +0000

    feat(rust2023): day16 pt02 - bit slow

--- rust/2023/src/bin/day16.rs
+++ rust/2023/src/bin/day16.rs
@@ -4,7 +4,10 @@ fn main() {
     let input = fs::read_to_string("./data/16.input").unwrap();
 
     let ans_pt1 = solve_pt1(&input);
-    println!("{}", ans_pt1);
+    println!("Part 1: {}", ans_pt1);
+
+    let ans_pt2 = solve_pt2(&input);
+    println!("Part 2 (bit slow): {}", ans_pt2);
 }
 
 fn solve_pt1(input: &str) -> usize {
@@ -23,7 +26,54 @@ fn solve_pt1(input: &str) -> usize {
         direction,
     );
 
-    activated.iter().map(|row| row.iter().sum::<usize>()).sum::<usize>()
+    activated
+        .iter()
+        .map(|row| row.iter().sum::<usize>())
+        .sum::<usize>()
+}
+
+fn solve_pt2(input: &str) -> usize {
+    let contraption_layout: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+
+    let mut possibles = vec![];
+
+    for direction in [[0, 1], [0, -1], [1, 0], [-1, 0]] {
+        let pos_range = if direction[0] == 0 {
+            0..contraption_layout[0].len()
+        } else {
+            0..contraption_layout.len()
+        };
+
+        for p in pos_range {
+            let mut activated =
+                vec![vec![0; contraption_layout[0].len()]; contraption_layout.len()];
+
+            let position = match direction {
+                [0, 1] => [p as i32, 0_i32],
+                [0, -1] => [p as i32, contraption_layout[0].len() as i32 - 1],
+                [1, 0] => [0_i32, p as i32],
+                [-1, 0] => [contraption_layout.len() as i32 - 1, p as i32],
+                _ => unreachable!(),
+            };
+
+            let mut visited: HashSet<([i32; 2], [i32; 2])> = HashSet::new();
+
+            recurse(
+                &mut visited,
+                &contraption_layout,
+                &mut activated,
+                position,
+                direction,
+            );
+            let this_config_ans = activated
+                .iter()
+                .map(|row| row.iter().sum::<usize>())
+                .sum::<usize>();
+            possibles.push(this_config_ans);
+        }
+    }
+
+    *possibles.iter().max().unwrap()
 }
 
 fn recurse(
@@ -62,12 +112,12 @@ fn recurse(
                 [1, 0] => {
                     recurse(visited, layout, activated, pos, [0, -1]);
                     recurse(visited, layout, activated, pos, [0, 1]);
-                    break
+                    break;
                 }
                 [-1, 0] => {
                     recurse(visited, layout, activated, pos, [0, -1]);
                     recurse(visited, layout, activated, pos, [0, 1]);
-                    break
+                    break;
                 }
                 _ => (),
             },
@@ -75,32 +125,32 @@ fn recurse(
                 [0, 1] => {
                     recurse(visited, layout, activated, pos, [1, 0]);
                     recurse(visited, layout, activated, pos, [-1, 0]);
-                    break
+                    break;
                 }
                 [0, -1] => {
                     recurse(visited, layout, activated, pos, [1, 0]);
                     recurse(visited, layout, activated, pos, [-1, 0]);
-                    break
+                    break;
                 }
                 _ => (),
             },
             _ => (),
         }
 
-//         for row in &mut *activated {
-//             println!(
-//                 "{}",
-//                 row.iter()
-//                     .map(|c| match c {
-//                         0 => '.',
-//                         1 => '#',
-//                         _ => unreachable!(),
-//                     })
-//                     .collect::<String>()
-//             );
-//         }
-//         println!("{:?}", pos);
-//         println!();
+        //         for row in &mut *activated {
+        //             println!(
+        //                 "{}",
+        //                 row.iter()
+        //                     .map(|c| match c {
+        //                         0 => '.',
+        //                         1 => '#',
+        //                         _ => unreachable!(),
+        //                     })
+        //                     .collect::<String>()
+        //             );
+        //         }
+        //         println!("{:?}", pos);
+        //         println!();
 
         pos[0] += dir[0];
         pos[1] += dir[1];
