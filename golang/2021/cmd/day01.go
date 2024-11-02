3c7bfd9091b204bacfd19ed11041863830bcade9
George Duffett <gduffett@certinia.com>
2024-11-02 11:09:38 +0000

    feat(golang2021): day01 pt2

--- golang/2021/cmd/day01.go
+++ golang/2021/cmd/day01.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
-	"os"
-
 )
 
 func main() {
@@ -16,26 +15,56 @@ func main() {
 
 	pt1 := part1(input)
 
-	fmt.Println("Part 1: " + strconv.Itoa(pt1));
-	fmt.Println("Part 2: " + "todo");
+	fmt.Println("Part 1: " + strconv.Itoa(pt1))
+
+	pt2 := part2(input)
+	fmt.Println("Part 2: " + strconv.Itoa(pt2))
 }
 
-func part1(input string) (int) {
+func part1(input string) int {
 
 	inputlines := strings.Split(input, "\n")
 
 	L := len(inputlines)
 	ints := make([]int, L)
 
-	for i := 0;i<L;i++ {
+	for i := 0; i < L; i++ {
 		si, err := strconv.Atoi(inputlines[i])
 		check(err)
 		ints[i] = si
 	}
 
 	tot := 0
-	for i := 0; i<(L-1);i++ {
-		if (ints[i+1] > ints[i]) {
+	for i := 0; i < (L - 1); i++ {
+		if ints[i+1] > ints[i] {
+			tot += 1
+		}
+	}
+
+	return tot
+}
+
+func part2(input string) int {
+	inputlines := strings.Split(input, "\n")
+
+	L := len(inputlines)
+	ints := make([]int, L)
+	for i := 0; i < L; i++ {
+		si, err := strconv.Atoi(inputlines[i])
+		check(err)
+		ints[i] = si
+	}
+
+	L -= 2
+	avgs := make([]int, L)
+	for i := 0; i < L; i++ {
+		avgs[i] = ints[i] + ints[i+1] + ints[i+2]
+	}
+
+	tot := 0
+	L -= 1
+	for i := 0; i < L; i++ {
+		if avgs[i+1] > avgs[i] {
 			tot += 1
 		}
 	}
