6b3fa5b843049f24caf4a559be8a648bcfdd87a6
George Duffett <gduffett@certinia.com>
2024-11-02 11:49:38 +0000

    chore(golang2021): add day template

---
+++ golang/2021/cmd/template/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Part 1: " + "todo")
+	fmt.Println("Part 2: " + "todo")
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
