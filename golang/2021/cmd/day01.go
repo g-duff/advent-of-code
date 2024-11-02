11cb69210a6073c7f600ea4e7d7b785272741c2b
George Duffett <gduffett@certinia.com>
2024-11-02 10:49:11 +0000

    feat(golang2021): day01 pt1

--- golang/2021/cmd/day01.go
+++ golang/2021/cmd/day01.go
@@ -2,9 +2,49 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+	"os"
+
 )
 
 func main() {
-	fmt.Println("Part 1: " + "todo");
+	file, err := os.ReadFile("./data/day01.input")
+	check(err)
+
+	input := strings.TrimSpace(string(file))
+
+	pt1 := part1(input)
+
+	fmt.Println("Part 1: " + strconv.Itoa(pt1));
 	fmt.Println("Part 2: " + "todo");
+}
+
+func part1(input string) (int) {
+
+	inputlines := strings.Split(input, "\n")
+
+	L := len(inputlines)
+	ints := make([]int, L)
+
+	for i := 0;i<L;i++ {
+		si, err := strconv.Atoi(inputlines[i])
+		check(err)
+		ints[i] = si
+	}
+
+	tot := 0
+	for i := 0; i<(L-1);i++ {
+		if (ints[i+1] > ints[i]) {
+			tot += 1
+		}
+	}
+
+	return tot
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
