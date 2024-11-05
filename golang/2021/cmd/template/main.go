b48d22b0f083c190cb9b30ac1f45bf2899750a89
George Duffett <gduffett@certinia.com>
2024-11-05 12:32:03 +0000

    chore(golang2021): update template

--- golang/2021/cmd/template/main.go
+++ golang/2021/cmd/template/main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Part 1: " + "todo")
-	fmt.Println("Part 2: " + "todo")
+	file, err := os.ReadFile("./data/day05.example")
+	input := strings.Split(strings.TrimSpace(string(file)), "\n")
+	check(err)
+
+	ansPt1 := solvePt1(input)
+	fmt.Println("Part 1:", ansPt1)
+
+	ansPt2 := solvePt2(input)
+	fmt.Println("Part 2:", ansPt2)
+}
+
+func solvePt1(input []string) int {
+	return 0
+}
+
+func solvePt2(input []string) int {
+	return 0
 }
 
 func check(e error) {
