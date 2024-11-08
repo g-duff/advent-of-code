0ae8fcf2fc8aebdadc7f148ece5de021787fa841
George Duffett <gduffett@certinia.com>
2024-11-08 20:50:59 +0000

    chore(golang2021): update template

--- golang/2021/cmd/template/main.go
+++ golang/2021/cmd/template/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+	"unicode"
 )
 
 func main() {
-	file, err := os.ReadFile("./data/day05.example")
-	input := strings.Split(strings.TrimSpace(string(file)), "\n")
+	file, err := os.ReadFile("./data/day07.example")
 	check(err)
+
+	input := parseInput(file)
 
 	ansPt1 := solvePt1(input)
 	fmt.Println("Part 1:", ansPt1)
@@ -18,12 +21,26 @@ func main() {
 	fmt.Println("Part 2:", ansPt2)
 }
 
-func solvePt1(input []string) int {
+func solvePt1(input []int) int {
 	return 0
 }
 
-func solvePt2(input []string) int {
+func solvePt2(input []int) int {
 	return 0
+}
+
+func parseInput(file []byte) []int {
+	input := string(file)
+
+	nums := strings.FieldsFunc(input, func(r rune) bool { return !unicode.IsNumber(r) })
+
+	parsed := make([]int, len(nums))
+	for i := range nums {
+		n, err := strconv.Atoi(nums[i])
+		check(err)
+		parsed[i] = n
+	}
+	return parsed
 }
 
 func check(e error) {
