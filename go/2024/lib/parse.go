be30504794c7605ead1cca2f7c7841e76db34cad
George <george.duffett@icloud.com>
2024-12-06 23:38:31 +0000

    feat(go2024): add parse int grid function to lib

---
+++ go/2024/lib/parse.go
@@ -0,0 +1,31 @@
+package lib
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func ParseToIntGrid(input []byte) [][]int {
+	data := strings.TrimSpace(string(input))
+	lines := strings.Split(data, "\n")
+
+	grid := make([][]int, len(lines))
+
+	for i, line := range lines {
+		fields := strings.FieldsFunc(line, func(r rune) bool { return !unicode.IsNumber(r) })
+		grid[i] = make([]int, len(fields))
+		for j, cell := range fields {
+			n, err := strconv.Atoi(cell)
+			check(err)
+			grid[i][j] = n
+		}
+	}
+	return grid
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
