931d88e6a01428779428574478525c905b717e4e
George <george.duffett@icloud.com>
2024-12-06 23:42:07 +0000

    feat(go2024): day02 pt1

---
+++ go/2024/cmd/day02/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/g-duff/advent-of-code/go/2024/lib"
+)
+
+func main() {
+	file, err := os.ReadFile("./data/day02.input")
+	check(err)
+
+	reports := lib.ParseToIntGrid(file)
+
+	pt1 := solvePt1(reports)
+	fmt.Println("Part 1: ", pt1)
+
+	pt2 := solvePt2(reports)
+	fmt.Println("Part 2: ", pt2)
+}
+
+func solvePt1(reports [][]int) int {
+	safeReports := 0
+	for _, report := range reports {
+		if isSafe(report) {
+			safeReports++
+		}
+	}
+	return safeReports
+}
+
+func solvePt2(reports [][]int) int {
+	return 0
+}
+
+func isSafe(report []int) bool {
+	isAllPositive := true
+	isAllNegative := true
+	isCorrectSize := true
+
+	N := len(report) - 1
+	for i := 0; i < N; i++ {
+		d := report[i] - report[i+1]
+
+		isAllPositive = isAllPositive && d >= 0
+		isAllNegative = isAllNegative && d < 0
+
+		if d < 0 {
+			d *= -1
+		}
+		isCorrectSize = isCorrectSize && (d >= 1 && d <= 3)
+	}
+
+	return isCorrectSize && (isAllPositive || isAllNegative)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
