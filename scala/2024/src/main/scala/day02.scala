b128fd9ce65c888b5a670d22891c2b8cfc7ccd67
George Duffett <george.duffett@bbc.co.uk>
2024-12-22 20:00:14 +0000

    feat(scala2024): day02 pt2

--- scala/2024/src/main/scala/day02.scala
+++ scala/2024/src/main/scala/day02.scala
@@ -9,18 +9,20 @@ val VALID_SIZES = Array(1, 2, 3)
   val pt1 = reports.count(isValid)
   println(s"Part 1: $pt1")
 
-  val pt2 = 0
+  val pt2 = reports.count(isValidWithTolerance)
   println(s"Part 2: $pt2")
 
 def parse(input: String): Array[Array[Int]] =
   input.split("\n").map(_.split(" ").map(_.toInt))
 
+def isValidWithTolerance(report: Array[Int]): Boolean =
+  (0 to report.size).map(report.patch(_, Nil, 1)).exists(isValid)
+
 def isValid(report: Array[Int]): Boolean =
   val diffs = report.sliding(2).map(w => w(1) - w(0)).toArray
 
-  val allCorrectSize =
-    diffs.map(VALID_SIZES contains Math.abs(_)).fold(true)(_ && _)
-  val allPositiveDiffs = diffs.map(_ > 0).fold(true)(_ && _)
-  val allNegativeDiffs = diffs.map(_ < 0).fold(true)(_ && _)
+  val allCorrectSize = diffs.forall(VALID_SIZES contains Math.abs(_))
+  val allPositiveDiffs = diffs.forall(_ > 0)
+  val allNegativeDiffs = diffs.forall(_ < 0)
 
   allCorrectSize && (allPositiveDiffs || allNegativeDiffs)
