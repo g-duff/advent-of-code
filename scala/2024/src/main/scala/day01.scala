155673789fd41f8f8ade1c6f5e5ad50d680dfe0e
George Duffett <george.duffett@bbc.co.uk>
2024-12-22 19:13:21 +0000

    refactor(scala2024): day01

--- scala/2024/src/main/scala/day01.scala
+++ scala/2024/src/main/scala/day01.scala
@@ -9,18 +9,14 @@ import scala.io
   col0 = col0.sortWith(_ < _)
   col1 = col1.sortWith(_ < _)
 
-  val pt1 = (col0 zip col1).map((c0, c1) => Math.abs(c1 - c0)).sum()
+  val pt1 = (col0 zip col1).map((c0, c1) => Math.abs(c1 - c0)).sum
   println(s"Part 1: $pt1")
 
   // Assume we'll use evaluated counts more than once
-  val counts = col0.groupMap(identity)(identity).mapValues(v => v.size)
-  val pt2 = col1.map(n => n * counts.getOrElse(n, 0)).sum()
+  val counts = col0.groupMap(identity)(identity).mapValues(_.size)
+  val pt2 = col1.map(n => n * counts.getOrElse(n, 0)).sum
   println(s"Part 2: $pt2")
 
 def parse(input: String): (Array[Int], Array[Int]) =
-  var lines = input.split("\n").map(line => line.split("   "))
-
-  var col0 = lines.map(cell => cell(0).toInt)
-  var col1 = lines.map(cell => cell(1).toInt)
-
-  (col0, col1)
+  var lines = input.split("\n").map(_.split("   "))
+  (lines.map(_(0).toInt), lines.map(_(1).toInt))
