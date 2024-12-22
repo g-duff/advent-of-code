d18ea2618ddd36530d18833f8e32df4c3e657cd5
George Duffett <george.duffett@bbc.co.uk>
2024-12-22 18:24:20 +0000

    feat(scala2024): day02 pt1

--- scala/2024/src/main/scala/day02.scala
+++ scala/2024/src/main/scala/day02.scala
@@ -1,8 +1,31 @@
 package day02
 
+val VALID_SIZES = Array(1, 2, 3)
+
 @main def solve(): Unit =
-  val pt1 = 0
+  val infile = io.Source.fromFile("./data/day02.input").mkString
+  var reports = parse(infile)
+
+  val isValidReports = reports.filter(isValid).size
+
+  val pt1 = isValidReports
   println(s"Part 1: $pt1")
 
   val pt2 = 0
   println(s"Part 2: $pt2")
+
+def parse(input: String): Array[Array[Int]] =
+  input.split("\n").map(_.split(" ").map(_.toInt))
+
+def isValid(report: Array[Int]): Boolean =
+  val diffs = report
+    .sliding(2)
+    .map(w => w(1) - w(0))
+    .toArray
+
+  val allCorrectSize =
+    diffs.map(VALID_SIZES contains Math.abs(_)).fold(true)(_ && _)
+  val allPositiveDiffs = diffs.map(_ > 0).fold(true)(_ && _)
+  val allNegativeDiffs = diffs.map(_ < 0).fold(true)(_ && _)
+
+  allCorrectSize && (allPositiveDiffs || allNegativeDiffs)
