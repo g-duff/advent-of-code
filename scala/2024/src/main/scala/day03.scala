bb8923e6800a254a10e6308205e9c24833f38299
George Duffett <george.duffett@bbc.co.uk>
2024-12-23 21:24:54 +0000

    feat(scala2024): day03 pt2

--- scala/2024/src/main/scala/day03.scala
+++ scala/2024/src/main/scala/day03.scala
@@ -13,5 +13,23 @@ import scala.util.matching.Regex
     .sum
   println(s"Part 1: $pt1")
 
+  val allPtn = """mul\((\d+),(\d+)\)|don't\(\)|do\(\)""".r
+  val matches = allPtn.findAllIn(infile)
+  var isActive = true
+  var pt2 = 0
+  for p <- matches.matchData do
+    p.toString match {
+      case "do()"    => isActive = true
+      case "don't()" => isActive = false
+      case _ =>
+        if isActive then
+          val m = mulPtn.findFirstMatchIn(p.toString)
+          m match {
+            case Some(m) => pt2 = pt2 + m.group(1).toInt * m.group(2).toInt
+            case _       =>
+          }
+    }
+  println(s"Part 2: $pt2")
+
 def parse(input: String): Array[Array[Int]] =
   input.split("\n").map(_.split(" ").map(_.toInt))
