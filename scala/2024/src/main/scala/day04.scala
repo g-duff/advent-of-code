72ea7bfd707d34370d6a4a11134cce97b3279972
George Duffett <george.duffett@bbc.co.uk>
2024-12-24 21:35:43 +0000

    refactor(scala2024): day04 make generic for pt2

--- scala/2024/src/main/scala/day04.scala
+++ scala/2024/src/main/scala/day04.scala
@@ -8,7 +8,10 @@ import scala.util.matching.Regex
   val R = infile.size
   val C = infile(0).size
 
-  val patterns = List(
+  val findPattern = (rOffset, cOffset, pattern) =>
+    findPatternIn(infile, R, C, rOffset, cOffset, pattern)
+
+  val pt1 = List(
     (0, 3, List((0, 0, 'X'), (0, 1, 'M'), (0, 2, 'A'), (0, 3, 'S'))),
     (0, 3, List((0, 0, 'S'), (0, 1, 'A'), (0, 2, 'M'), (0, 3, 'X'))),
     (3, 0, List((0, 0, 'X'), (1, 0, 'M'), (2, 0, 'A'), (3, 0, 'S'))),
@@ -17,23 +20,25 @@ import scala.util.matching.Regex
     (3, 3, List((0, 0, 'S'), (1, 1, 'A'), (2, 2, 'M'), (3, 3, 'X'))),
     (3, 3, List((0, 3, 'X'), (1, 2, 'M'), (2, 1, 'A'), (3, 0, 'S'))),
     (3, 3, List((0, 3, 'S'), (1, 2, 'A'), (2, 1, 'M'), (3, 0, 'X')))
-  )
-
-  val pt1 = patterns.map { (rOffset, cOffset, pattern) =>
-    val coords =
-      (0 to R - rOffset - 1).flatMap(r =>
-        (0 to C - cOffset - 1).map(c => (r, c))
-      )
-
-    coords.count { (r, c) =>
-      pattern.forall { (charR, charC, char) =>
-        infile(r + charR)(c + charC) == char
-      }
-    }
-
-  }.sum
-
+  ).map(findPattern.tupled).sum
   println(s"Part1: $pt1")
 
 def parse(input: String): Array[Array[Char]] =
   input.split("\n").map(_.toCharArray())
+
+def findPatternIn(
+    infile: Array[Array[Char]],
+    R: Int,
+    C: Int,
+    rOffset: Int,
+    cOffset: Int,
+    pattern: List[(Int, Int, Char)]
+) =
+  val coords =
+    (0 to R - rOffset - 1).flatMap(r => (0 to C - cOffset - 1).map(c => (r, c)))
+
+  coords.count { (r, c) =>
+    pattern.forall { (charR, charC, char) =>
+      infile(r + charR)(c + charC) == char
+    }
+  }
