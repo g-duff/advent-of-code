3c5d44bb4cc8ff26a02c6f1f54cec49030ba091f
George Duffett <george.duffett@bbc.co.uk>
2025-12-01 13:03:06 +0000

    chore(ts2025): day01 format

--- typescript/2025/README.md
+++ typescript/2025/README.md
@@ -1,3 +1,3 @@
-# AoC 2025 in TypeScript 
+# AoC 2025 in TypeScript
 
 These solutions were written to work with [Node.js >25 native type-stripping](https://nodejs.org/docs/latest-v25.x/api/typescript.html#type-stripping)
--- typescript/2025/src/day01.ts
+++ typescript/2025/src/day01.ts
@@ -1,9 +1,7 @@
-import { readFile } from "node:fs/promises";
+import { readFile } from 'node:fs/promises';
 
-
-const infile = await readFile("data/day01.input", {encoding: "utf-8"})
+const infile = await readFile('data/day01.input', { encoding: 'utf-8' });
 const rows = infile.trim().split('\n');
-
 
 // Horrible
 var state = 50;
@@ -11,29 +9,29 @@ var state = 50;
 var pt1 = 0;
 var pt2 = 0;
 rows.forEach((row) => {
-	const [dir, ...countStr] = row;
-	const count = parseInt(countStr.join(''));
+  const [dir, ...countStr] = row;
+  const count = parseInt(countStr.join(''));
 
-	if (dir == "R")
-		for (var n=0; n<count;n++){
-			state += 1;
-			if (state % 100 == 0) pt2++;
-		}
-	else 	
-		for (var n=0; n<count;n++){
-			state -= 1;
-			if (state % 100 == 0) pt2++;
-		}
+  if (dir == 'R')
+    for (var n = 0; n < count; n++) {
+      state += 1;
+      if (state % 100 == 0) pt2++;
+    }
+  else
+    for (var n = 0; n < count; n++) {
+      state -= 1;
+      if (state % 100 == 0) pt2++;
+    }
 
-	if (state < 0) {
-		state = (state%100) + 100;
-	}
-	if (state > 99) {
-		state = (state%100);
-	}
+  if (state < 0) {
+    state = (state % 100) + 100;
+  }
+  if (state > 99) {
+    state = state % 100;
+  }
 
-	if (state == 0) pt1++;
-	return state;
+  if (state == 0) pt1++;
+  return state;
 });
 
 console.log(`Part 1: ${pt1}`);
